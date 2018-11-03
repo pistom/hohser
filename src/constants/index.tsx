835d152d8a8ac3b82a9c21b7523eab6ed9afcf8b
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-03 11:17:49 +0100

    feature: add promise middleware to redux

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -7,5 +7,17 @@ export type FULL_HIDE = typeof FULL_HIDE;
 export const ADD_DOMAIN = 'ADD_DOMAIN';
 export type ADD_DOMAIN = typeof ADD_DOMAIN;
 
+export const FETCH_DOMAINS = 'FETCH_DOMAINS';
+export type FETCH_DOMAINS = typeof FETCH_DOMAINS;
+
+export const FETCH_DOMAINS_PENDING = 'FETCH_DOMAINS_PENDING';
+export type FETCH_DOMAINS_PENDING = typeof FETCH_DOMAINS_PENDING;
+
+export const FETCH_DOMAINS_FULFILLED = 'FETCH_DOMAINS_FULFILLED';
+export type FETCH_DOMAINS_FULFILLED = typeof FETCH_DOMAINS_FULFILLED;
+
 export const REMOVE_DOMAIN = 'REMOVE_DOMAIN';
 export type REMOVE_DOMAIN = typeof REMOVE_DOMAIN;
+
+export const TEST = 'TEST';
+export type TEST = typeof TEST;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,22 +1,24 @@
 import App from '../components/App/App';
-import * as actions from '../actions/';
-import { StoreState } from '../types/index';
+import * as actions from '../actions';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { FULL_HIDE, PARTIAL_HIDE } from 'src/constants';
+import { DomainsState } from 'src/reducers/domains';
 
 
-export function mapStateToProps({ domainsList, settings }: StoreState) {
+export function mapStateToProps({ domainsList, option, dog }: DomainsState) {
   return {
     domainsList,
-    settings
+    option,
+    dog
   }
 }
 
-export function mapDispatchToProps(dispatch: Dispatch<actions.DomainAction>) {
+export function mapDispatchToProps(dispatch: Dispatch<actions.DomainAction | actions.OptionAction>) {
   return {
     addDomain: (domainName: string, hideType: FULL_HIDE | PARTIAL_HIDE) => dispatch(actions.addDomain(domainName, hideType)),
     removeDomain: () => dispatch(actions.removeDomain()),
+    fetchDomainsList: () => dispatch(actions.fetchDomainsList())
   }
 }
 
