e258d6d6a1450b0ae616184cc09516355b2a6c18
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-12 21:48:23 +0100

    add MuiThemeProvider

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,6 +6,17 @@ import { Provider } from 'react-redux';
 import promise from 'redux-promise-middleware';
 import BrowserStorageSyncMock from './mock/BrowserStorageSyncMock';
 import { reducers } from './reducers';
+import { MuiThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: green,
+  },
+});
 
 /*
  * This constant stores a reference to browser.storage.sync object.
@@ -18,7 +29,9 @@ const store = createStore(reducers, applyMiddleware(promise()));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
