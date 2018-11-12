5d437ce249dc04ef1dd08aad9bed0ccccac1e7da
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-12 21:48:23 +0100

    add MuiThemeProvider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -16,7 +16,17 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  appBarBottom: {
+    top: 'auto',
+    bottom: 0,
+  },
+  appBarTop: {
+    bottom: 'auto',
+    top: 0,
+  },
 };
+
+
 
 export interface Props {
   domainsList: Array<Domain>;
@@ -37,7 +47,7 @@ class App extends React.Component<Props> {
     this.props.fetchDomainsList();
   }
 
-  componentWillUpdate (nextProps: Props){
+  componentWillUpdate (nextProps: Props) {
     if (this.props.domainsListLoading && !nextProps.domainsListLoading) {
       this.props.importFromOldVersion();
     }
@@ -49,10 +59,9 @@ class App extends React.Component<Props> {
 
   public render () {
     const { classes } = this.props;
-    return (
-      <React.Fragment>
-      <CssBaseline />
-        <AppBar position="static">
+    return [
+      <CssBaseline />,
+      <AppBar position="fixed" color="primary" className={classes.appBarTop}>
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
@@ -61,8 +70,9 @@ class App extends React.Component<Props> {
             Highlight or Hide
           </Typography>
         </Toolbar>
-      </AppBar>
-        <AddDomain addDomain={this.props.addDomain} />
+      </AppBar>,
+      <AddDomain addDomain={this.props.addDomain} />,
+      <div style={{paddingTop: 48}}>
         {Object.keys(this.props.domainsList).map((item, i) => (
           <li key={i}>
             {this.props.domainsList[item].domainName} -
@@ -70,9 +80,79 @@ class App extends React.Component<Props> {
             {this.props.domainsList[item].color ? <span>{this.props.domainsList[item].color}</span> : null} -
             <span onClick={() => this.removeDomainHandle(i)}>❌</span>
           </li>
-        ))}
-    </React.Fragment>
-    );
+        ))
+        }
+      </div>,
+      <AppBar position="fixed" color="default" className={classes.appBarBottom}>
+        <Toolbar>
+          <form className={classes.root} autoComplete="off">
+            <Grid container spacing={8}>
+              <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  label="Domain"
+                  id="margin-none"
+                  className={classes.textField}
+
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel htmlFor="age-simple">Style</InputLabel>
+                  <Select
+                    // value={this.state.age}
+                    // onChange={this.handleChange}
+                    inputProps={{
+                      name: 'Style',
+                      id: 'age-simple',
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel htmlFor="age-simple">Color</InputLabel>
+                  <Select
+                    // value={this.state.age}
+                    // onChange={this.handleChange}
+                    inputProps={{
+                      name: 'Color',
+                      id: 'age-simple',
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs>
+                <FormControl margin="dense" fullWidth className={classes.formControl}>
+                  <div style={{textAlign: 'right'}}>
+                    <Button variant="fab" mini color="secondary" aria-label="Add" className={classes.button}>
+                      <AddIcon />
+                    </Button>
+                  </div>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </form>
+
+        </Toolbar>
+
+      </AppBar>
+    ];
   }
 }
 
