89c82fa6c6541a540e6ccad1e2ac394ada340921
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-13 20:56:58 +0100

    separate BottomBar from App component

---
+++ src/components/BottomBar/BottomBar.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import { Toolbar, AppBar, withStyles, Grid, FormControl, TextField, InputLabel, Select, MenuItem, Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+
+interface Props {
+  classes: any;
+}
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  appBar: {
+    bottom: 'auto',
+    top: 0,
+  },
+};
+
+const BottomBar = (props: Props) => {
+  const classes = props.classes;
+  return(
+    <AppBar position="fixed" color="default" className={classes.appBar}>
+        <Toolbar>
+          <form className={classes.root} autoComplete="off">
+            <Grid container spacing={8}>
+              <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  label="Domain"
+                  id="margin-none"
+                  className={classes.textField}
+
+                />
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel htmlFor="age-simple">Style</InputLabel>
+                  <Select
+                    // value={this.state.age}
+                    // onChange={this.handleChange}
+                    inputProps={{
+                      name: 'Style',
+                      id: 'age-simple',
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel htmlFor="age-simple">Color</InputLabel>
+                  <Select
+                    // value={this.state.age}
+                    // onChange={this.handleChange}
+                    inputProps={{
+                      name: 'Color',
+                      id: 'age-simple',
+                    }}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs>
+                <FormControl margin="dense" fullWidth className={classes.formControl}>
+                  <div style={{textAlign: 'right'}}>
+                    <Button variant="fab" mini color="secondary" aria-label="Add" className={classes.button}>
+                      <AddIcon />
+                    </Button>
+                  </div>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </form>
+
+        </Toolbar>
+
+      </AppBar>
+  );
+};
+
+export default withStyles(styles)(BottomBar);
