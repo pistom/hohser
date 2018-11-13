4cf118614a98b8a144874652585a4aee5c049ed6
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-13 20:56:58 +0100

    separate BottomBar from App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,11 @@
 import { Domain, DisplayStyle } from 'src/types';
 import * as React from 'react';
 import AddDomain from '../AddDomain/AddDomain';
-import AppBar from '@material-ui/core/AppBar';
-import { Toolbar, withStyles, CssBaseline, TextField, Select, MenuItem, FormControl, InputLabel, Button, Grid } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 
-import AddIcon from '@material-ui/icons/Add';
+
 import TopBar from '../TopBar/TopBar';
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  appBarBottom: {
-    top: 'auto',
-    bottom: 0,
-  },
-  appBarTop: {
-    bottom: 'auto',
-    top: 0,
-  },
-};
-
-
+import BottomBar from '../BottomBar/BottomBar';
 
 export interface Props {
   domainsList: Array<Domain>;
@@ -37,7 +14,6 @@ export interface Props {
   removeDomain: (index: number) => void;
   fetchDomainsList: () => void;
   importFromOldVersion: () => void;
-  classes: any;
 }
 
 class App extends React.Component<Props> {
@@ -60,7 +36,6 @@ class App extends React.Component<Props> {
   }
 
   public render () {
-    const { classes } = this.props;
     return [
       <CssBaseline />,
       <TopBar />,
@@ -76,77 +51,9 @@ class App extends React.Component<Props> {
         ))
         }
       </div>,
-      <AppBar position="fixed" color="default" className={classes.appBarBottom}>
-        <Toolbar>
-          <form className={classes.root} autoComplete="off">
-            <Grid container spacing={8}>
-              <Grid item xs={6}>
-              <FormControl fullWidth>
-                <TextField
-                  label="Domain"
-                  id="margin-none"
-                  className={classes.textField}
-
-                />
-                </FormControl>
-              </Grid>
-              <Grid item xs={2}>
-                <FormControl fullWidth className={classes.formControl}>
-                  <InputLabel htmlFor="age-simple">Style</InputLabel>
-                  <Select
-                    // value={this.state.age}
-                    // onChange={this.handleChange}
-                    inputProps={{
-                      name: 'Style',
-                      id: 'age-simple',
-                    }}
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs={2}>
-                <FormControl fullWidth className={classes.formControl}>
-                  <InputLabel htmlFor="age-simple">Color</InputLabel>
-                  <Select
-                    // value={this.state.age}
-                    // onChange={this.handleChange}
-                    inputProps={{
-                      name: 'Color',
-                      id: 'age-simple',
-                    }}
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs>
-                <FormControl margin="dense" fullWidth className={classes.formControl}>
-                  <div style={{textAlign: 'right'}}>
-                    <Button variant="fab" mini color="secondary" aria-label="Add" className={classes.button}>
-                      <AddIcon />
-                    </Button>
-                  </div>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </form>
-
-        </Toolbar>
-
-      </AppBar>
+      <BottomBar />
     ];
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
