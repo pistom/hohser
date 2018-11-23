87d70d78f3e17fe6c8fa0f640bb82e6538c230da
tomaszpisarek <topisarek-ext@airfrance.fr>
2018-11-23 16:54:35 +0100

    feature: add ResultManagement component

--- src/config/duckduckgo.ts
+++ src/config/duckduckgo.ts
@@ -2,6 +2,6 @@ import { SearchEngineConfig } from 'src/types';
 
 export const duckduckgo: SearchEngineConfig = {
   resultSelector: '.result',
-  domainSelector: '.result__url__domain',
+  domainSelector: '.result__url',
   observerSelector: '#links'
 };
