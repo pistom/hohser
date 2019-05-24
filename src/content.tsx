d194443a763df5695364202cb3ae7356a8255457
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2019-05-24 20:10:04 +0200

    bugfix: change reprocess results way

--- src/content.tsx
+++ src/content.tsx
@@ -19,92 +19,95 @@ let searchEngineConfig: SearchEngineConfig = config[searchEngine];
 // Array of management component anchors
 const managementComponentAnchors: Array<Element> = [];
 
-// Number of attempts to process results
-let processResultsAttempt: number = 0;
-
 // Process results function
 async function processResults () {
+  const resultsList = document.querySelectorAll(
+    searchEngineConfig.resultSelector
+  );
+
+  // Fetching domains list and options
+  const domainsList = await storageManager.fetchDomainsList();
+  const options = await storageManager.fetchOptions();
+
+  // Clear managementComponent anchors
+  managementComponentAnchors.forEach(a => {
+    try{
+      if (a.parentNode) a.parentNode.removeChild(a);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  resultsList.forEach(r => {
+    // Number of attempts to process results
+    let processResultsAttempt: number = 0;
+    processResult(r, domainsList, options, processResultsAttempt);
+  });
+}
+
+// Process one result
+function processResult (r: Element, domainsList: any, options: any, processResultsAttempt: number): void {
   try {
-    const resultsList = document.querySelectorAll(
-      searchEngineConfig.resultSelector
+    const result = r as HTMLElement;
+    result.classList.add('hohser_result');
+    const domain = result.querySelector(
+      searchEngineConfig.domainSelector
+    ) as HTMLElement;
+    // Skip result if no domain selector
+    if (!domain) return;
+
+    const url = domain.innerText;
+    if (!url) {
+      throw "No domain info";
+    }
+
+    // Add management component to the result
+    const managementComponentAnchor = result.appendChild(document.createElement("span"));
+    managementComponentAnchor.classList.add("hohser_result_management");
+    managementComponentAnchors.push(managementComponentAnchor);
+    ReactDOM.render(
+      <ResultManagement url={url} />,
+      managementComponentAnchor as HTMLElement
     );
 
-    // Fetching domains list and options
-    const domainsList = await storageManager.fetchDomainsList();
-    const options = await storageManager.fetchOptions();
-
-    // Clear managementComponent anchors
-    managementComponentAnchors.forEach(a => {
-      try{
-        if (a.parentNode) a.parentNode.removeChild(a);
-      } catch (e) {
-        console.log(e);
+    // Listen to management results buttons click and event stop propagation
+    managementComponentAnchor.addEventListener('click', (e: any) => {
+      e.preventDefault();
+      e.stopPropagation();
+      let action;
+      let color;
+      let domain;
+      const nodeName = e.target.nodeName;
+      if (e.target && nodeName === 'BUTTON') {
+        action = e.target.dataset.action;
+        color = e.target.dataset.color;
+        domain = e.target.dataset.domain;
+      } else if (e.target && nodeName === 'SVG' || nodeName === 'svg') {
+        action = e.target.parentNode.parentNode.dataset.action;
+        color = e.target.parentNode.parentNode.dataset.color;
+        domain = e.target.parentNode.parentNode.dataset.domain;
+      } else if (e.target && nodeName === 'PATH' || nodeName === 'path') {
+        action = e.target.parentNode.parentNode.parentNode.dataset.action;
+        color = e.target.parentNode.parentNode.parentNode.dataset.color;
+        domain = e.target.parentNode.parentNode.parentNode.dataset.domain;
       }
+      storageManager.save(domain, action, color);
     });
 
-    resultsList.forEach(r => {
-      const result = r as HTMLElement;
-      result.classList.add('hohser_result');
-      const domain = result.querySelector(
-        searchEngineConfig.domainSelector
-      ) as HTMLElement;
-      // Skip result if no domain selector
-      if (!domain) return;
-
-      const url = domain.innerText;
-
-      if (!url) {
-        throw "No domain info";
-      }
-
-      // Add management component to the result
-      const managementComponentAnchor = result.appendChild(document.createElement("span"));
-      managementComponentAnchor.classList.add("hohser_result_management");
-      managementComponentAnchors.push(managementComponentAnchor);
-      ReactDOM.render(
-        <ResultManagement url={url} />,
-        managementComponentAnchor as HTMLElement
-      );
-
-      // Listen to management results buttons click and event stop propagation
-      managementComponentAnchor.addEventListener('click', (e: any) => {
-        e.preventDefault();
-        e.stopPropagation();
-        let action;
-        let color;
-        let domain;
-        const nodeName = e.target.nodeName;
-        if (e.target && nodeName === 'BUTTON') {
-          action = e.target.dataset.action;
-          color = e.target.dataset.color;
-          domain = e.target.dataset.domain;
-        } else if (e.target && nodeName === 'SVG' || nodeName === 'svg') {
-          action = e.target.parentNode.parentNode.dataset.action;
-          color = e.target.parentNode.parentNode.dataset.color;
-          domain = e.target.parentNode.parentNode.dataset.domain;
-        } else if (e.target && nodeName === 'PATH' || nodeName === 'path') {
-          action = e.target.parentNode.parentNode.parentNode.dataset.action;
-          color = e.target.parentNode.parentNode.parentNode.dataset.color;
-          domain = e.target.parentNode.parentNode.parentNode.dataset.domain;
-        }
-        storageManager.save(domain, action, color);
-      });
-
-      // Add or remove classes to the matches results
-      const matches = domainsList.filter((s: Domain) => url.includes(s.domainName));
-      if (matches.length > 0) {
-        removeResultStyle(result);
-        applyResultStyle(result, matches[0].color, matches[0].display, options);
-      } else {
-        removeResultStyle(result);
-      }
-    });
+    // Add or remove classes to the matches results
+    const matches = domainsList.filter((s: Domain) => url.includes(s.domainName));
+    if (matches.length > 0) {
+      removeResultStyle(result);
+      applyResultStyle(result, matches[0].color, matches[0].display, options);
+    } else {
+      removeResultStyle(result);
+    }
   } catch (e) {
-    console.error(e);
-    // Try to process results again
+    console.warn(e);
+    // Try to process result again
     if (++processResultsAttempt <= 3) {
       setTimeout(() => {
-        processResults();
+        processResult(r, domainsList, options, processResultsAttempt);
       }, 100 * Math.pow(processResultsAttempt, 3));
     }
   }
