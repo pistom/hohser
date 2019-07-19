dbc55e5a1759b5644cb838d9626670665d37b235
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2019-07-19 20:20:13 +0200

    feat: upgrade dependencies

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -200,7 +200,6 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
-                        minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
                     },
