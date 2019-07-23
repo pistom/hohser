c5df2242bca26d89b3acf79cfcdcf89468552fbb
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2019-07-23 08:53:23 +0200

    feat: add media query and google mobile selectors

--- src/config/google.ts
+++ src/config/google.ts
@@ -1,7 +1,7 @@
 import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
-  resultSelector: '.g',
-  domainSelector: '.TbwUpd',
-  observerSelector: '#rcnt'
+  resultSelector: '.g, .mnr-c',
+  domainSelector: '.TbwUpd, .dTe0Ie',
+  observerSelector: '#rcnt, #cnt'
 };
