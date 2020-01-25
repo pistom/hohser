15fd934547391b08ed11f73508bdbe54c3fa46cd
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2020-01-25 18:30:13 +0100

    fix: add new selectors to google config

--- src/config/google.ts
+++ src/config/google.ts
@@ -1,7 +1,7 @@
 import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
-  resultSelector: '.g, .mnr-c',
-  domainSelector: '.TbwUpd, .dTe0Ie',
-  observerSelector: '#rcnt, #cnt'
+  resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc',
+  domainSelector: '.TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .BNeawe',
+  observerSelector: '#rcnt, #cnt, #rg, #main'
 };
