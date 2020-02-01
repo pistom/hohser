46128bd9d89a7e6475786f40c380ae834f21116a
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2020-02-01 07:56:12 +0100

    feat: add new google news selectors to google config

--- src/config/google.ts
+++ src/config/google.ts
@@ -1,7 +1,7 @@
 import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
-  resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc',
-  domainSelector: '.TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .BNeawe',
+  resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc, .JP1Bwd',
+  domainSelector: '.TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .BNeawe, .pDavDe',
   observerSelector: '#rcnt, #cnt, #rg, #main'
 };
