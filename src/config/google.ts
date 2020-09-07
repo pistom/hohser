2afc8c4ba42320a02738ad07a755977e0e006b4f
yzrsng <usgrgs@gmail.com>
2020-09-07 11:13:16 +0900

    fix: add google mobile selector for Kiwi Browser on Android
    It didn't work before. It works now.
    Test User-Agent: Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36

--- src/config/google.ts
+++ src/config/google.ts
@@ -2,7 +2,7 @@ import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
   resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc, .JP1Bwd, .isv-r',
-  domainSelector: '.TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .UPmit, .pDavDe, .fxgdke',
+  domainSelector: '.TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .UPmit, .gBIQub, .pDavDe, .fxgdke',
   observerSelector: '#rcnt, #cnt, #rg, #main',
   resultUrlSelector: '.r > a'
 };
