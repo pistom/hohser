c3aee3f3ffd9c9533a5335845bbd2205e532f368
Tomasz Pisarek <tomasz@bigsool.com>
2024-08-13 15:31:54 +0200

    fix: update startpage selectors

--- src/config/startpage.ts
+++ src/config/startpage.ts
@@ -1,7 +1,7 @@
 import { SearchEngineConfig } from '../types';
 
 export const startpage: SearchEngineConfig = {
-  resultSelector: '.search-result, .w-gl__result',
-  domainSelector: '.search-item__url, .w-gl__result-url',
-  observerSelector: '.column--main__content > ol',
+  resultSelector: '.search-result, .w-gl__result, .result',
+  domainSelector: '.search-item__url, .w-gl__result-url, .link-text',
+  observerSelector: '.column--main__content > ol, #main',
 };
