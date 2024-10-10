04129b15a3a77267830ce52a9644bcac79287c9f
Tomasz Pisarek <tomasz@bigsool.com>
2024-10-10 08:42:15 +0200

    feat: fix google yt results

--- src/config/google.ts
+++ src/config/google.ts
@@ -2,7 +2,7 @@ import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
   resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc, .JP1Bwd, .isv-r, .cUezCb, .KZmu8e, .EQ4p8c, .sh-dlr__list-result',
-  domainSelector: '.yuRUbf a, .TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .UPmit, .gBIQub, .pDavDe, .fxgdke, .fxYMc, .IHk3ob, .E5ocAb, .lg3aE',
+  domainSelector: '.yuRUbf a, .TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .UPmit, .gBIQub, .pDavDe, .fxgdke, .fxYMc, .IHk3ob, .E5ocAb, .lg3aE, [jsname="UWckNb"]',
   observerSelector: '#rcnt, #cnt, #rg, #main',
   resultUrlSelector: '.r > a',
   ajaxResults: true,
