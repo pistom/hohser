d4cb032389ccd2bf20400915c06e1d0be0265e56
Tomasz Pisarek <tomasz@bigsool.com>
2025-03-24 09:05:37 +0100

    fix: google, startpage configs

--- src/config/google.ts
+++ src/config/google.ts
@@ -1,9 +1,9 @@
 import { SearchEngineConfig } from '../types';
 
 export const google: SearchEngineConfig = {
-  resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc, .JP1Bwd, .isv-r, .cUezCb, .KZmu8e, .EQ4p8c, .sh-dlr__list-result',
+  resultSelector: '.g, .mnr-c, .rg_bx, .rg_di, .rg_el, .ZINbbc, .JP1Bwd, .isv-r, .cUezCb, .KZmu8e, .EQ4p8c, .sh-dlr__list-result, .wHYlTd',
   domainSelector: '.yuRUbf a, .TbwUpd, .dTe0Ie, .xQ82C, .e8fRJf, .FnqxG, .UPmit, .gBIQub, .pDavDe, .fxgdke, .fxYMc, .IHk3ob, .E5ocAb, .lg3aE, [jsname="UWckNb"]',
-  observerSelector: '#rcnt, #cnt, #rg, #main',
+  observerSelector: '#rcnt, #cnt, #rg, #main, #res',
   resultUrlSelector: '.r > a',
   ajaxResults: true,
 };
--- src/config/startpage.ts
+++ src/config/startpage.ts
@@ -2,6 +2,6 @@ import { SearchEngineConfig } from '../types';
 
 export const startpage: SearchEngineConfig = {
   resultSelector: '.search-result, .w-gl__result, .result',
-  domainSelector: '.search-item__url, .w-gl__result-url, .link-text',
+  domainSelector: '.search-item__url, .w-gl__result-url, .structured-link-text > span',
   observerSelector: '.column--main__content > ol, #main',
 };
